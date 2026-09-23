eab01915424e349a593e2fa8a6e6d4201d3019e7
AhmedSaad-EGY <ahmedsaad20169799@gmail.com>
2026-09-23 19:15:41 +0300

    chore: use brand logo as favicon

--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -1,6 +1,13 @@
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from 'react-router'
 
+import logoMark from './assets/brand/logo-mark.png'
 import './styles/index.css'
+
+// React Router requires this route-module export to add the favicon to <head>.
+// eslint-disable-next-line react-refresh/only-export-components
+export const links = () => [
+  { rel: 'icon', type: 'image/png', href: logoMark },
+]
 
 export default function App() {
   return (
